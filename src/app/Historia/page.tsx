72e7f242bb41f28fa80948962c04b3e7347201f2
DaniM0613 <wwh.rj.92@gmail.com>
2025-07-30 18:40:05 -0600

    Adding another update regarding the styles

--- src/app/Historia/page.tsx
+++ src/app/Historia/page.tsx
@@ -7,34 +7,28 @@ export default function Historia() {
   return (
     <section
       id="historia"
-      className="flex flex-col items-center justify-center min-h-screen gap-2"
+      className="flex flex-col items-center justify-center min-h-screen gap-2 px-8"
     >
-      <div>
-        <h2 className="text-4xl font-extrabold text-center text-[#0c2601]">
-          Historia
-        </h2>
+      <div className="flex flex-row max-sm:flex max-sm:flex-wrap px-4 gap-2 items-center justify-center">
+        {information.map((info) => (
+          <div
+            key={info.id}
+            className="grid grid-cols md:grid-cols-2 sm:grid-cols-1 px-8 gap-6 w-full"
+          >
+            <h1 className="text-orange-4 text-base sm:text-xl rounded-2xl w-full h-full p-6 bg-gradient-to-b from-[#a1e7d9]/100 to-[#9a430a]/60 text-center">
+              {" "}
+              {info.description}{" "}
+            </h1>
 
-        <div className="flex flex-row max-sm:flex max-sm:flex-wrap px-4 py-2 items-center justify-center ">
-          {information.map((info) => (
-            <div
-              key={info.id}
-              className="grid grid-cols-2 md:grid-cols-2 gap-4"
-            >
-              <h1 className="text-orange-4 text-sm sm:text-2xl rounded-2xl md:text-3xl w-full p-6 bg-gradient-to-b from-[#a1e7d9]/100 to-[#9a430a]/60 to-99%">
-                {" "}
-                {info.description}{" "}
-              </h1>
-
-              <Image
-                src={info.image}
-                alt="image"
-                className="col-start-2 row-start-2 rounded-2xl"
-                width={600}
-                height={600}
-              />
-            </div>
-          ))}
-        </div>
+            <Image
+              src={info.image}
+              alt="image"
+              className="w-full h-auto rounded-2xl object-cover md:col-start-2 md:row-start-2"
+              width={600}
+              height={600}
+            />
+          </div>
+        ))}
       </div>
     </section>
   );

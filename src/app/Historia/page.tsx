b654da1cbfcdb04dac338afd3f827f1e0ff86af6
DaniM0613 <wwh.rj.92@gmail.com>
2025-07-31 16:56:36 -0600

    new update

--- src/app/Historia/page.tsx
+++ src/app/Historia/page.tsx
@@ -5,31 +5,25 @@ import { information } from "./data/information";
 
 export default function Historia() {
   return (
-    <section
-      id="historia"
-      className="flex flex-col items-center justify-center min-h-screen gap-2 px-8"
-    >
-      <div className="flex flex-row max-sm:flex max-sm:flex-wrap px-4 gap-2 items-center justify-center">
-        {information.map((info) => (
-          <div
-            key={info.id}
-            className="grid grid-cols md:grid-cols-2 sm:grid-cols-1 px-8 gap-6 w-full"
-          >
-            <h1 className="text-orange-4 text-base sm:text-xl rounded-2xl w-full h-full p-6 bg-gradient-to-b from-[#a1e7d9]/100 to-[#9a430a]/60 text-center">
-              {" "}
-              {info.description}{" "}
-            </h1>
-
-            <Image
-              src={info.image}
-              alt="image"
-              className="w-full h-auto rounded-2xl object-cover md:col-start-2 md:row-start-2"
-              width={600}
-              height={600}
-            />
+      <section id="historia" className="flex flex-col items-center justify-center min-h-screen px-8 sm:px-8">
+       
+          <div className="flex flex-col w-full max-w-screen-lg md:px-8">
+            {information.map((info) => (
+              <div key={info.id} className="relative w-full ">
+                <Image
+                  src={info.image}
+                  alt=""
+                  width={600}
+                  height={500}
+                  className="rounded-xl h-[500px]"
+                />
+                 
+                <div className="relative md:absolute md:left-[75%] md:transform md:-translate-x-1/2 h-[250px] md:h-[550px] bottom-4 w-[80%] md:w-[50%] mx-auto -mt-40  text-black rounded-xl p-10 shadow-xl backdrop-blur-xl bg-gradient-to-b from-[#65d3bd]/90 via-[#da6c13]/50 to-[#9a430a]/70">
+                  <p className="text-sm text-center">{info.description}</p>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-    </section>
+        </section>
   );
 }

f0dcf4b63f141af77bf69c7bf056182b7494c538
GlendyT <glendytuyuc@gmail.com>
2025-10-31 00:25:38 -0600

    checking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 import { poppins } from "@/utils/Fonts";
 import Inicio from "./inicio/Inicio";
 import Patrocinadores from "./patrocinadores/Patrocinadores";
-import Historia from "./historia/Historia";
 import Nosotras from "./nosotras/Nosotras";
 import Album from "./album/Album";
 import Videos from "./album/Videos";
@@ -11,6 +10,7 @@ import Experencia from "./(experencia)/Experiencia";
 import Participantes from "./participantes/Participantes";
 import Musicos from "./musicos/Musicos";
 import Footer from "@/utils/Footer";
+import Historia from "./historia/page";
 
 export default function Home() {
   return (

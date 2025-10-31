bf241b7ef36260d9c31ed558573fdf72c55c8ea8
GlendyT <glendytuyuc@gmail.com>
2025-10-31 00:03:56 -0600

    editing the ts config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,23 @@
 "use client";
-import Footer from "@/utils/Footer";
+
+import { poppins } from "@/utils/Fonts";
 import Inicio from "./inicio/page";
-import Musicos from "./musicos/page";
-import Nosotras from "./nosotras/page";
-import Participantes from "./participantes/page";
 import Patrocinadores from "./patrocinadores/page";
+import Historia from "./historia/page";
+import Nosotras from "./nosotras/page";
 import Album from "./album/page";
 import Videos from "./album/Videos";
 import Experencia from "./(experencia)/Experiencia";
-import { poppins } from "@/utils/Fonts";
-import Historia from "./historia/page";
+import Participantes from "./participantes/page";
+import Musicos from "./musicos/page";
+import Footer from "@/utils/Footer";
 
 export default function Home() {
   return (
     <main className={` ${poppins.className} `}>
       <Inicio />
       <Patrocinadores />
-      <Historia/>
+      <Historia />
       <Nosotras />
       <Album />
       <Videos />

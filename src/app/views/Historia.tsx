fb0c57e05be9c15b6628fccff2f0e19c6e7f8286
GlendyT <glendytuyuc@gmail.com>
2025-10-31 10:59:04 -0600

    trying to fix the videos and album route2

--- src/app/views/Historia.tsx
+++ src/app/views/Historia.tsx
@@ -1,7 +1,7 @@
 "use client";
 
+import { information } from "@/constants/information";
 import Image from "next/image";
-import { information } from "../historia/data/information";
 
 export default function Historia() {
   return (
